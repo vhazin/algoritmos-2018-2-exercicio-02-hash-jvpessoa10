5209b945d9b82c084f16bebb96b2471f0dc87b66
João Victor Pessoa <jvpessoa10@hotmail.com>
2018-08-22 11:18:32 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -4,7 +4,6 @@
 /*
 t
 2
-
 a,b,x,n,c,d,m
 2 3 1 3 0 1 7
 1 0 0 8 0 8 9
@@ -14,15 +13,15 @@ int a,b,x,n,c,d;
 long unsigned int m;
 int counter = 0;
 
-
 int main(){
     scanf("%d",&t);
     int result[t];
     for(int i = 0;i<t;i++){
         scanf("%d %d %d %d %d %d %lu", &a, &b, &x, &n, &c, &d, &m);
+        
         for(long unsigned int j = 0;j<=n;j++){
-             
-            if((a*(x+j)+b)%m>=c && (a*(x+j)+b)%m<=d){
+            int res = (a*(x+j)+b)%m;
+            if(res>=c && res<=d){
                 counter++;
             }
             
